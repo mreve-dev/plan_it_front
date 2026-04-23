068eca4856c96af7e91e4d8942c53075658d912e
morgane reveau <morganereveau11@gmail.com>
2026-04-23 17:24:24 +0200

    Première interface de login (page d'accueil)

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,8 +1,26 @@
+import { MdOutlineSettings } from "react-icons/md";
+
+
 const LoginPage = () => {
   return (
-    <div>
-      
+    <div className="h-screen flex flex-col bg-amber-50 p-3">
+      <div className="h-[8%] flex justify-end items-center" >
+        <MdOutlineSettings size={55} color="#4f9288"/>
+      </div>
+
+      <div className="h-[92%] flex flex-col justify-center items-center gap-8">
+        <figure className="w-90">
+          <img src="./src/assets/logo_classic.png" alt="" />
+        </figure>
+        <button className="btn border-0 bg-[#cd5090] w-50">Se connecter</button>
+      </div>
+
+
+      <div className="flex justify-center">
+        <button className="btn border-0 bg-[#4f9288] w-50">A propos</button>
+      </div>
     </div>
+
   )
 }
 
